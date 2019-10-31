2035890a896adae6ecc7e420fe9e3dbc10091019
Richard Leven <richard.leven@udo.edu>
2019-10-31 14:04:29 +0100

    Change parameters, correct output
    Data was triple checked!!!

--- hook.py
+++ hook.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+x = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50]
 print(x)
 F = [0.15, 0.29, 0.44, 0.59, 0.74, 0.89, 1.04, 1.19, 1.34, 1.49]
 print(F)
@@ -12,4 +12,4 @@ for i in range(0, 10):
     a[i] = x[i]/F[i]
 
 b = np.sum(a)
-print(a, b/10)
+print('Der Mittelwert der Federkonstante beträgt: ', b/10, 'kg/s^2.')
