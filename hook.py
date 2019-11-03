790952927b58ec1d5224757636d94c4269334a2b
Richard Leven <richard.leven@udo.edu>
2019-11-03 18:18:43 +0100

    Add errors, smoothed out code
    To implement errors was not necessary, given that all errors of x are exactly 5mm
    but it's good practice nonetheless.

--- hook.py
+++ hook.py
@@ -1,20 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
 x = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50]
-print(x)
+e_x = [0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005]
 F = [0.15, 0.29, 0.44, 0.59, 0.74, 0.89, 1.04, 1.19, 1.34, 1.49]
-print(F)
-
+e_F = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+m, n, r, p, std = linregress(x, F)
+print(m, n, r, p, std)
+
+for i in range(0, 10):
+    y[i] = m*x[i]+n
+
 
 for i in range(0, 10):
     a[i] = x[i]/F[i]
 
-b = np.sum(a)
-print('Der Mittelwert der Federkonstante beträgt: ', b/10, 'kg/s^2.')
+b = np.sum(a)/10
+print('Der Mittelwert der Federkonstante beträgt: ', b, 'kg/s^2.')
 
-plt.plot(x, F, '.', label = 'Messdaten')
+plt.errorbar(x, F, xerr=e_x, yerr=e_F, fmt='b.', label='Messdaten')
+plt.plot(x, y, c='red', label = 'Ausgleichsgerade')
 plt.xlabel(r"$x$ in [m]")
 plt.ylabel(r"$F$ in [N]")
 plt.legend(loc = 'best')
