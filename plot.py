359a0ab0f5c691e5edc1f845463a0cc4d02e9b95
Richard Leven <richard.leven@udo.edu>
2019-12-10 09:58:58 +0100

    Add anhang and improved auswertung

--- plot.py
+++ plot.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 R = 30
-C = 5e-6
+C = 5e-9
 L = 0.0035
 x = [0, 25e-6, 54e-6, 78e-6, 105e-6, 130e-6, 158e-6, 184e-6, 210e-6, 247e-6]
 y = [0.2, 0.14, 0.1, 0.07, 0.05, 0.04, 0.03, 0.02, 0.014, 0.008]
 a = np.linspace(0, 250e-6, 1000)
-b = 0.2*np.exp(-R/(2*L)*a)
+b = 0.2*np.exp(-(R*a)/(2*L))
 #plt.subplot(1, 2, 1)
 plt.plot(x, y, '+r', label='Messwerte')
 plt.plot(a, b, '-g', label='Vergleich')
