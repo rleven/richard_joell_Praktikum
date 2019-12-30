db5d2f9d3072387dbe4a0b067e7b26aebe005458
Richard Leven <richard.leven@udo.edu>
2019-12-30 10:34:38 +0100

    Final version of Gekoppelte Schwingungen

--- Gekoppelte_Schwingungen[done]/berechnungen.py
+++ Gekoppelte_Schwingungen[done]/berechnungen.py
@@ -21,12 +21,14 @@ w2 = [0, 0, 0, 0, 0, 0, 0]
 v1 = [39990, 37275, 35730, 34760, 33870, 33260, 32740]
 v2 = [0, 0, 0, 0, 0, 0, 0]
 ref = [0, 0, 0, 0, 0, 0, 0]
+ref2 = [0, 0, 0, 0, 0, 0, 0]
 
 while i<7:
     w1[i] = lomegaplus(C_k[i])
     w2[i] = lomegaminus(C_k[i])
     v2[i] = vminus(C_k[i])
     ref[i] = (1-v1[i]/v2[i])*100
+    ref2[i] = v2[i]/192001.5
     i+=1
 
 
@@ -36,5 +38,6 @@ print("L- : ", w2)
 print("Approximation: ", 1/(2*R))
 I2 = 8*0.01
 print("Finalwerte für I2: ", I2)
-print("v- Werte: ", vminus(1.01e-9))
+print("v- Werte: ", v2)
 print("Abweichung: ", (1-47090/vminus(1.01e-9))*100)
+print("Verhältnis: ", ref2)
